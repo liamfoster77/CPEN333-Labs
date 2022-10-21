8a7d3e0be986b0af231c7f601f34d2326576e87f
liamfoster77 <liamfoster77@gmail.com>
2022-10-20 17:58:47 -0700

    Completed lab 5 and added improved variant

--- Lab05Better.py
+++ Lab05Better.py
@@ -1,6 +1,15 @@
 #student name: Liam Foster
 #student number: 40199382
 
+"""
+    I have changed the template code in the following ways:
+
+    - The waitForItemToBeConsumed function is changed so that "item" is no longer an argument
+    - The debug messages are moved into the try block of the code so that they only print if the item is actually
+    inserted or removed from the buffer
+"""
+
+
 import threading
 import random #is used to cause some randomness 
 import time   #is used to cause some delay in item production/consumption
@@ -50,19 +59,19 @@ def producer() -> None:
 
     for _ in range(SIZE * 2): #we just produce twice the buffer size for testing
         item = waitForItemToBeProduced()  #wait for an item to be produced
-        print(f"DEBUG: {item} produced")
+        #print(f"DEBUG: {item} produced")
         #complete the function below here to correctly store the item in the circular buffer
         
-        #check if there is space in the buffer
-        if empty._value != 0:
-            mutex.acquire()                     #acquire lock
-            try:
+        mutex.acquire()                     #acquire lock
+        try:
+            #check if there is space in the buffer
+            if empty._value != 0:
                 buffer.insert(item)             #insert item into buffer
                 full.release()                  #increment full
                 empty.acquire()                 #decrement empty
-                #print(f"DEBUG: {item} produced")
-            finally:
-                mutex.release()                 #release lock no matter what
+                print(f"DEBUG: {item} produced")
+        finally:
+            mutex.release()                 #release lock no matter what
 
 
 def consumer() -> None:
@@ -70,55 +79,46 @@ def consumer() -> None:
         Implement the consumer function to be used by the consumer thread.
         It must correctly use full, empty and mutex.
     """
+    def waitForItemToBeConsumed() -> None: #inner function; use as is
+            time.sleep(round(random.uniform(.1, .3), 2)) #a random delay (100 to 300 ms)
+            #to simulate consumption, item is thrown away here by just ignoring it
+    
     for _ in range(SIZE * 2): #we just consume twice the buffer size for testing
         #write the code below to correctly remove an item from the circular buffer
         
-        """
-            The consumer must wait to be started after the producer. This is because the debug message is
-            specified to be left after item removal from the buffer, so it will print the debug message
-            regardless if the buffer is actually changed or not. Without a wait between starting the producer
-            and consumer, item will be called by the waitForItemToBeConsumed function and the print statement
-            even though it hasn't been defined yet and this causes an error.
-        """
-
-        #check that there are items to remove
-        if full._value != 0:
-            mutex.acquire()                     #acquire lock
-            try:
+        mutex.acquire()                     #acquire lock
+        try:
+            #check that there are items to remove
+            if full._value != 0:
                 item = buffer.remove()          #remove item from buffer
                 full.acquire()                  #decrement full
                 empty.release()                 #increment empty
-                #print(f"DEBUG: {item} consumed")
-            finally:
-                mutex.release()                 #release lock no matter what
-
+                print(f"DEBUG: {item} consumed")
+        finally:
+            mutex.release()                 #release lock no matter what
+        
         #end of your implementation for this function
         #use the following code as is
-        def waitForItemToBeConsumed(item) -> None: #inner function; use as is
-            time.sleep(round(random.uniform(.1, .3), 2)) #a random delay (100 to 300 ms)
-            #to simulate consumption, item is thrown away here by just ignoring it
-        waitForItemToBeConsumed(item)  #wait for the item to be consumed
-        print(f"DEBUG: {item} consumed")
+        
+        waitForItemToBeConsumed()  #wait for the item to be consumed
+        #print(f"DEBUG: {item} consumed")
 
         
 
 if __name__ == "__main__":
     SIZE = 5  #buffer size
-    buffer = circularBuffer(SIZE)  #initialize the buffer
+    buffer = circularBuffer(SIZE)   #initialize the buffer
 
     full = threading.Semaphore(0)       #full semaphore: number of full buffers
                                         #initial value set to 0
     empty = threading.Semaphore(SIZE)   #empty semaphore: number of empty buffers
                                         #initial value set to SIZE
-    mutex = threading.Lock()            #lock for protecting data on insertion or removal
+    mutex = threading.Lock()    #lock for protecting data on insertion or removal
 
     #complete the producer-consumer thread creation below
     producerThread = threading.Thread(target=producer)
     producerThread.start()
     consumerThread = threading.Thread(target=consumer)
-    #sleep for a half sec before starting consumer thread to make sure the buffer has 
-    #elements to remove. See consumer thread comment for details
-    time.sleep(0.5)
     consumerThread.start()
     producerThread.join()
     consumerThread.join()
